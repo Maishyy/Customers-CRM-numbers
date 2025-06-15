91668a3ea7628d5eb28be033f9b2b740b972018a
Maishyy <leonmwangi16@gmail.com>
2025-06-15 13:39:46 +0300

    Migrate Firebase creds to Streamlit secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ import pdfplumber
 from io import BytesIO
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import os
 
-# --- Firebase Init ---
-cred = credentials.Certificate("contact-extractor-app-firebase-adminsdk-fbsvc-a9022e904a.json")
+firebase_json = json.loads(st.secrets["firebase_creds"])
+cred = credentials.Certificate(firebase_json)
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 db = firestore.client()
